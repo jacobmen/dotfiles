915566b577c53412d9791d35d22b4ed163505262
Jacob Men <jacobmen416@yahoo.com>
2022-03-18 10:37:50 -0500

    add initialization script

---
+++ scripts/config-init.sh
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+set -euxo pipefail
+
+git clone --bare https://github.com/jacobmen/dotfiles.git $HOME/.dotfiles
+
+function config {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+config checkout
+
+if [ $? = 0 ]; then
+    echo "Checked out dotfiles"
+else
+    echo "Moving existing dotfiles to ~/.dotfiles-backup"
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi
+
+config checkout
+config config status.showUntrackedFiles no
