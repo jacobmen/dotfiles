15c977c76b2c82f415bfd88f96a785673330fd66
Jacob Men <40839083+jacobmen@users.noreply.github.com>
2023-06-26 21:07:07 -0700

    Update gitsigns

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,28 @@
 vim.opt.showmatch = false
+
 vim.opt.relativenumber = true
+vim.opt.nu = true
+
+vim.opt.incsearch = true
 vim.opt.hlsearch = false
 vim.opt.hidden = true
 vim.opt.errorbells = false
+
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
 vim.opt.autoindent = true
-vim.opt.nu = true
 vim.opt.wrap = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
 vim.opt.undofile = true
-vim.opt.incsearch = true
+
 vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.splitright = true
