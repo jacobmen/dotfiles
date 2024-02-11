15f8dece278017b0d7a43d896f2a059a70b90211
Jacob Men <40839083+jacobmen@users.noreply.github.com>
2024-02-10 22:13:01 -0800

    - move on_attach function to single use location

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -125,7 +125,34 @@ return {
                 -- default handler
                 function(server_name)
                     lspconfig[server_name].setup({
-                        on_attach = require("lsp_utils").on_attach,
+                        on_attach = function(client, bufnr)
+                            --Enable completion triggered by <c-x><c-o>
+                            vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+                            -- Mappings.
+                            local opts = { noremap = true, silent = true, buffer = bufnr }
+
+                            -- See `:help vim.lsp.*` for documentation on any of the below functions
+                            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+                            vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
+                            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                            vim.keymap.set("i", "<C-k>", "<cmd>Lspsaga signature_help<CR>", opts)
+                            vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+                            vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                            vim.keymap.set("n", "<leader>wl", function()
+                                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                            end, opts)
+                            vim.keymap.set("n", "<leader>rr", "<cmd>Lspsaga rename<CR>", opts)
+                            vim.keymap.set("n", "<leader>ga", "<cmd>Lspsaga code_action<CR>", opts)
+                            vim.keymap.set("n", "<leader>gr", "<cmd>Lspsaga lsp_finder<CR>", opts)
+                            vim.keymap.set("n", "<leader>f", function()
+                                vim.lsp.buf.format({ async = true })
+                            end, opts)
+
+                            vim.keymap.set("n", "<leader>gn", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+                            vim.keymap.set("n", "<leader>gN", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+                            vim.keymap.set("n", "<leader>cc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+                        end,
                         capabilities = server_name == "clangd" and { offsetEncoding = "utf-8" } or capabilities,
                     })
                 end,
